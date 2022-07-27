15b04c9686e84bd93203ec9834ecf2a246a8cdd6
farzin <farzin-nasiri@hotmail.com>
2022-07-27 23:40:59 +0430

    change import path

--- benchmarks/setup.go
+++ benchmarks/setup.go
@@ -1,3 +1,5 @@
+//go:build exclud
+
 package benchmarks
 
 import (
@@ -5,8 +7,9 @@ import (
 	"fmt"
 	"log"
 
+	dv1 "github.com/digiexpress/dlocator/internal/pkg/api"
+
 	"github.com/digiexpress/dlocator/internal/app/dlocator"
-	dv1 "github.com/digiexpress/dlocator/pkg/api/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -31,3 +34,31 @@ func setupClient(ctx context.Context, app *dlocator.App) dv1.CourierLocatorClien
 
 	return dv1.NewCourierLocatorClient(conn)
 }
+
+//func TestServer(t *testing.T) dv1.CourierLocatorClient {
+//	conn, err := grpc.DialContext(
+//		ctx,
+//		fmt.Sprintf("localhost:%d", 9000), grpc.WithTransportCredentials(insecure.NewCredentials()),
+//	)
+//	if err != nil {
+//		log.Panicf("could not dial grpc server on port %d", 9000)
+//	}
+//
+//	ctx, cancel := context.WithCancel(context.Background())
+//	client := dv1.NewCourierLocatorClient(conn)
+//	sim := NewCourierMovementSimulation(20_000)
+//	go sim.Run(ctx, client)
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		_, err := client.FindNearbyCouriers(ctx, &dv1.FindNearbyCouriersQuery{
+//			Target: randomLocationInTehran(),
+//			Radius: &dv1.Distance{Amount: randomFloat64(50, 500), Unit: dv1.Unit_Meters},
+//		})
+//		if err != nil {
+//			b.Fail()
+//		}
+//	}
+//	//
+//	cancel()
+//
+//}
